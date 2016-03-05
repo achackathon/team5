6524b3af727983abb70531a5a6a008cd59be6903
JonathanF07 <jonathanf07@gmail.com>
2016-03-05 14:45:27 -0300

    Creating controller and refactoring.

---
+++ src/main/java/accessible/controller/AccessibilityCategoryController.java
@@ -0,0 +1,60 @@
+package accessible.controller;
+
+import accessible.dao.AccessibilityCategoryDAO;
+import accessible.model.AccessibilityCategory;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author jfranco
+ */
+public class AccessibilityCategoryController {
+    
+    private final Result result;
+    private final AccessibilityCategoryDAO accessibilityCategoryDAO;
+
+    public AccessibilityCategoryController() {
+        this.result = null;
+        this.accessibilityCategoryDAO = null;
+    }
+    
+    @Inject
+    public AccessibilityCategoryController(Result result, AccessibilityCategoryDAO placesDAO) {
+        this.result = result;
+        this.accessibilityCategoryDAO = placesDAO;
+    }
+
+    @Get
+    @Path({"accessibilityCategory/", "accessibilityCategory"})
+    public void list() {
+        List accessibilityCategories = accessibilityCategoryDAO.list();
+        result.use(Results.json()).withoutRoot().from(accessibilityCategories).serialize();
+    }
+
+    @Post
+    @Path({"accessibilityCategory/", "accessibilityCategory"})
+    public void insert(AccessibilityCategory accessibilityCategory) {
+        accessibilityCategoryDAO.insert(accessibilityCategory);
+        result.nothing();
+    }
+
+    @Get
+    @Path({"accessibilityCategory/{id}"})
+    public void load(Integer id) {
+        AccessibilityCategory accessibilityCategory = accessibilityCategoryDAO.load(id);
+        result.use(Results.json()).from(accessibilityCategory).serialize();
+    }
+
+    @Post
+    @Path({"accessibilityCategory/{id}"})
+    public void update(AccessibilityCategory accessibilityCategory, Integer id) {
+        accessibilityCategoryDAO.update(accessibilityCategory, id);
+    }
+    
+}
