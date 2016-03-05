3b759409d0e6d94e8a540720c04bf2f636d302e5
Maison Chaves <maison@nsrv.com.br>
2016-03-05 19:08:36 -0300

    Accessibility

--- src/main/java/accessible/dao/AccessibilityDAO.java
+++ src/main/java/accessible/dao/AccessibilityDAO.java
@@ -2,7 +2,6 @@
 package accessible.dao;
 
 import accessible.model.Accessibility;
-import accessible.model.Commentary;
 import accessible.template.AccessibilityTemplate;
 import br.com.six2six.fixturefactory.Fixture;
 import java.util.ArrayList;
@@ -18,7 +17,7 @@ public  class AccessibilityDAO {
     
     public AccessibilityDAO() {
         AccessibilityTemplate.load();
-        list = Fixture.from(Commentary.class).gimme(2, "valid");
+        list = Fixture.from(Accessibility.class).gimme(2, "valid");
     }
 
     List<Accessibility> list = new ArrayList<>();
