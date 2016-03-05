68c7bbc599ba387ba01093b912a0ef11d14b0b4e
Maison Chaves <maison@nsrv.com.br>
2016-03-05 19:11:34 -0300

    correção

--- src/main/webapp/assets/js/controllers/registerController.js
+++ src/main/webapp/assets/js/controllers/registerController.js
@@ -1,118 +1,126 @@
-(function (eaApp, angular, $) {
-    'use strict';
+(function(eaApp, angular, $) {
+	'use strict';
 
-    eaApp.controller('registerController', function ($scope, registerService) {
-        $scope.showAddNewItem = false;
+	eaApp.controller('registerController', function($scope, registerService, accessibilityItems, categories) {
+		$scope.showAddNewItem = false;
 
-        $scope.accessibilityItems = [
-            {
-                name: 'rampa'
-            },
-            {
-                name: 'menu em braile'
-            },
-            {
-                name: 'garçom que conhece libras'
-            },
-            {
-                name: 'garçom que conhece libras'
-            },
-            {
-                name: 'garçom que conhece libras'
-            },
-            {
-                name: 'garçom que conhece libras'
-            }
-        ];
+		$scope.accessibilityItems = accessibilityItems.data;
 
-        $scope.categories = [
-            {
-                name: 'mobilidade reduzida'
-            },
-            {
-                name: 'deficiente visual'
-            },
-            {
-                name: 'deficiente auditivo'
-            }
-        ];
+		$scope.categories = categories.data;
 
-        $scope.obj = {
-            accessibilityItems: []
-        };
-        $scope.newItem = {
-            categories: []
-        };
+		$scope.obj = {
+			accessibilityItem: []
+		};
+		$scope.newItem = {
+			categories: []
+		}
 
-        $scope.cancelAddNewItem = function () {
-            $scope.showAddNewItem = false;
+		$scope.cancelAddNewItem = function() {
+			$scope.showAddNewItem = false;
 
-            $scope.newItem = {
-                categories: []
-            };
+			$scope.newItem = {
+				categories: []
+			};
 
-            var i;
-            for (i = 0; i < $scope.categories.length; i++) {
-                $scope.categories[i].checked = false;
-            }
-        };
-        $scope.displayAddNewItem = function () {
-            $scope.showAddNewItem = true;
-        };
+			var i;
+			for(i=0; i<$scope.categories.length; i++) {
+				$scope.categories[i].checked = false;
+			}
+		};
+		$scope.displayAddNewItem = function() {
+			$scope.showAddNewItem = true;
+		};
 
-        $scope.cancelAddCategory = function () {
-            $scope.showAddCategory = false;
-        };
-        $scope.displayAddCategory = function () {
-            $scope.showAddCategory = true;
-        };
+		$scope.cancelAddCategory = function() {
+			$scope.showAddCategory = false;
 
-        $scope.handleNewItem = function (item) {
-            if (item.checked) {
-                $scope.obj.accessibilityItems.push(item);
-            } else {
-                $scope.removeItem(item.name);
-            }
-        };
-        $scope.handlerCategory = function (category) {
-            if (category.checked) {
-                $scope.newItem.categories.push(category);
-            } else {
-                $scope.removeCategory(category);
-            }
-        };
+			$scope.newItem.newCategory = "";
+		};
+		$scope.displayAddCategory = function() {
+			$scope.showAddCategory = true;
+		};
 
-        $scope.removeCategory = function (category) {
-            var i = $scope.newItem.categories.length;
+		$scope.handleNewItem = function(item) {
+			if(item.checked) {
+				$scope.obj.accessibilityItem.push({
+					accessibility: item
+				});
+			}
+			else {
+				$scope.removeItem(item.name);
+			}
+		};
+		$scope.handlerCategory = function(category) {
+			if(category.checked) {
+				$scope.newItem.categories.push(category);
+			}
+			else {
+				$scope.removeCategory(category);
+			}
+		};
 
-            while (i--) {
-                if ($scope.newItem.categories[i] == category) {
-                    $scope.newItem.categories.splice(i, 1);
-                }
-            }
-        };
+		$scope.removeCategory = function(category) {
+			var i = $scope.newItem.categories.length;
 
-        $scope.removeItem = function (name) {
-            var i = $scope.obj.accessibilityItems.length;
+			while(i--) {
+				if($scope.newItem.categories[i] == category) {
+					$scope.newItem.categories.splice(i, 1);
+				}
+			}
+		};
 
-            while (i--) {
-                if ($scope.obj.accessibilityItems[i].name == name) {
-                    $scope.obj.accessibilityItems.splice(i, 1);
-                }
-            }
-        };
+		$scope.removeItem = function(name) {
+			var i = $scope.obj.accessibilityItem.length;
 
-        $scope.addNewItem = function () {
-            registerService.addNewItem($scope.newItem).success(function (data) {
+			while(i--) {
+				if($scope.obj.accessibilityItem[i].accessibility.name == name) {
+					$scope.obj.accessibilityItem.splice(i, 1);
+				}
+			}
+		};
 
-                $scope.accessibilityItems.unshift({
-                    name: $scope.newItem.name,
-                    checked: true
-                });
+		$scope.addNewItem = function() {
+			registerService.addNewItem($scope.newItem).success(function(data) {
+				var item = {
+					name: $scope.newItem.name,
+					checked: true
+				};
 
-                $scope.cancelAddNewItem();
-            });
-        };
-    });
+				$scope.accessibilityItems.unshift(item);
+				$scope.handleNewItem(item);
+
+				$scope.cancelAddNewItem();
+			});
+		};
+
+		$scope.addCategory = function() {
+			registerService.addCategory({name: $scope.newItem.newCategory}).success(function(data) {
+
+				$scope.categories.unshift({
+					name: $scope.newItem.newCategory,
+					checked: true
+				});
+
+				$scope.cancelAddCategory();
+			});
+		};
+
+		$scope.addEstablishment = function() {
+			var i;
+
+			for(i=0; i<$scope.obj.accessibilityItem.length; i++) {
+				$scope.obj.accessibilityItem[i].grade = $scope.obj.accessibilityItem[i].accessibility.grade;
+			}
+			registerService.addEstablishment($scope.obj).success(function(data) {
+
+				$scope.categories.unshift({
+					name: $scope.newItem.newCategory,
+					checked: true
+				});
+
+				$scope.cancelAddCategory();
+			});
+		}
+	});
 
 })(angular.module('eaApp'), window.angular, window.jQuery);
